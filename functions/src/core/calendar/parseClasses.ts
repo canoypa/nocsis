d60a407eff28eff785c12092e86c465f678ea155
Cano <contact.sasasaky@gmail.com>
2022-07-22 09:16:53 +0900

    using new calendar model

--- functions/src/core/calendar/parseClasses.ts
+++ functions/src/core/calendar/parseClasses.ts
@@ -1,18 +1,19 @@
+/* eslint-disable camelcase */
 import { calendar_v3 as CalendarV3 } from "@googleapis/calendar";
-import { CalendarClass } from "../../models/class";
+import { NEW_CalendarClass } from "../../models/class";
 import { parseEventDate } from "./parseEventDate";
 
 export const parseClasses = (
   items: CalendarV3.Schema$Event[]
-): CalendarClass[] => {
+): NEW_CalendarClass[] => {
   return items.map((v, i) => {
     const date = parseEventDate(v);
 
-    return new CalendarClass({
+    return {
       startAt: date.start,
       endAt: date.end,
       title: v.summary || "(No title)",
       period: i + 1,
-    });
+    };
   });
 };
--- functions/src/core/calendar/parseEvents.ts
+++ functions/src/core/calendar/parseEvents.ts
@@ -1,20 +1,21 @@
+/* eslint-disable camelcase */
 import { calendar_v3 as CalendarV3 } from "@googleapis/calendar";
-import { CalendarEvent } from "../../models/event";
+import { NEW_CalendarEvent } from "../../models/event";
 import { parseEventDate } from "./parseEventDate";
 
 export const parseEvents = (
   items: CalendarV3.Schema$Event[]
-): CalendarEvent[] => {
+): NEW_CalendarEvent[] => {
   return items.map((v) => {
     const date = parseEventDate(v);
 
-    return new CalendarEvent({
+    return {
       startAt: date.start,
       endAt: date.end,
       title: v.summary || "(No title)",
       description: v.description || "",
       location: v.location || "",
       isAllDay: v.start?.dateTime === undefined,
-    });
+    };
   });
 };
