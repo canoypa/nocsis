67e89ea4d4f496b222659057829ade2a2383e818
Cano <contact.sasasaky@gmail.com>
2022-09-16 10:42:54 +0900

    make applayout

--- personal/lib/main.dart
+++ personal/lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'package:nocsis_personal/core/router.dart';
 import 'package:nocsis_personal/core/theme.dart';
 import 'package:nocsis_personal/firebase_options.dart';
+import 'package:nocsis_personal/pages/layout.dart';
 
 void main() async {
   // url をパス形式にする
@@ -31,36 +30,7 @@ class MyApp extends StatelessWidget {
       darkTheme: darkTheme,
       routeInformationParser: router.parser,
       routerDelegate: router.delegate,
-      builder: (context, child) {
-        return Title(
-          title: "Nocsis",
-          color: Theme.of(context).colorScheme.surface,
-          // 必要な初期処理が終わるまでアプリを表示しない
-          child: FutureBuilder(
-            future: Future.wait(
-              [
-                // DateTime の日本語フォーマット初期化
-                initializeDateFormatting("ja_JP"),
-                // 最初の認証までサインインしてないことにされるので待つ
-                FirebaseAuth.instance.authStateChanges().first,
-              ],
-            ),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return child!;
-              }
-
-              return Scaffold(
-                body: Center(
-                  child: snapshot.hasError
-                      ? const Text("Something went wrong.")
-                      : const CircularProgressIndicator(),
-                ),
-              );
-            },
-          ),
-        );
-      },
+      builder: (_, child) => AppLayout(child: child!),
     );
   }
 }
---
+++ personal/lib/pages/layout.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+class AppLayout extends StatelessWidget {
+  final Widget child;
+
+  const AppLayout({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Title(
+      title: "Nocsis",
+      color: Theme.of(context).colorScheme.surface,
+      // 必要な初期処理が終わるまでアプリを表示しない
+      child: FutureBuilder(
+        future: Future.wait(
+          [
+            // DateTime の日本語フォーマット初期化
+            initializeDateFormatting("ja_JP"),
+            // 最初の認証までサインインしてないことにされるので待つ
+            FirebaseAuth.instance.authStateChanges().first,
+          ],
+        ),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return child;
+          }
+
+          return Scaffold(
+            body: Center(
+              child: snapshot.hasError
+                  ? const Text("Something went wrong.")
+                  : const CircularProgressIndicator(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
