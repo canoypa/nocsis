a61a4ee1fc672ac96d70b43582093d40df7152dd
Cano <contact.sasasaky@gmail.com>
2022-09-20 09:23:24 +0900

    move basic card to components/

--- personal/lib/widget/class_list.dart
+++ personal/lib/widget/class_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:nocsis_personal/components/basic_card.dart';
 import 'package:nocsis_personal/models/calendar_class/calendar_class.dart';
-import 'package:nocsis_personal/widget/basic_card.dart';
 
 class ClassList extends StatelessWidget {
   final List<CalendarClass> items;
