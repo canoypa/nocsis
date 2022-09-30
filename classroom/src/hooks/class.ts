41d3a6c3dd2b4d6dc97d28bc1a12d731dc276b1e
Cano <developer.sasazono@gmail.com>
2022-09-30 14:45:52 +0900

    useDuringClass の改善 (#26)

--- classroom/src/hooks/class.ts
+++ classroom/src/hooks/class.ts
@@ -1,53 +1,47 @@
+import { Interval } from "luxon";
 import { useCallback, useState } from "react";
-import { useRecoilValueLoadable } from "recoil";
+import { useRecoilValue } from "recoil";
 import { ClassesSelector } from "../atoms/classes";
 import { getNewDate } from "../core/date";
 import { ClassData } from "../types/classes";
 import { useIntervalEffect } from "./date";
 
-type UseClasses = () => {
-  /** 授業中か */
-  during: boolean;
-  /** 現行の授業 */
-  current: ClassData | null;
-};
+export type DuringClassState =
+  | { during: true; classData: ClassData }
+  | { during: false; classData: null };
 
-/** 進行中の授業があるか管理する hook */
-export const useDuringClass: UseClasses = () => {
-  const { state, contents: classes } = useRecoilValueLoadable(ClassesSelector);
+/** 進行中の授業を確認する hook */
+export const useDuringClass = (): DuringClassState => {
+  const classes = useRecoilValue(ClassesSelector);
 
-  const [during, setDuring] = useState<boolean>(false);
-  const [current, setCurrent] = useState<ClassData | null>(null);
+  const [state, setState] = useState<DuringClassState>({
+    during: false,
+    classData: null,
+  });
 
   // 進行中の授業があるかチェック
   const checkCurrentClass = useCallback(() => {
-    if (state !== "hasValue") {
+    if (classes.isEmpty) {
+      setState({ during: false, classData: null });
       return;
     }
 
-    if (!classes.isEmpty) {
-      const date = getNewDate();
+    const date = getNewDate();
 
-      // 進行中の授業を取得、なければ null
-      const currentClass = classes.items.find(
-        (v) =>
-          0 <= date.diff(v.startAt).as("milliseconds") &&
-          date.diff(v.endAt).as("milliseconds") <= 0
-      );
+    const currentClass = classes.items.find((v) =>
+      // date が授業時間内かのチェック
+      // endAt は排他的なので -1 しとく
+      Interval.fromDateTimes(v.startAt, v.endAt.minus(1)).contains(date)
+    );
 
-      if (currentClass) {
-        setCurrent(currentClass);
-        setDuring(true);
-      } else {
-        setDuring(false);
-      }
+    if (currentClass) {
+      setState({ during: true, classData: currentClass });
+    } else {
+      setState({ during: false, classData: null });
     }
   }, [classes]);
 
   useIntervalEffect(checkCurrentClass, "minute", [checkCurrentClass]);
 
-  return {
-    during,
-    current,
-  };
+  return state;
 };
