0516ee7cf35f851dd24f29b53e9e38d77dd91fd4
Cano <developer.sasazono@gmail.com>
2022-11-16 09:59:10 +0900

    add root error boundary (#34)

---
+++ classroom/src/components/root_error_boundary.tsx
@@ -0,0 +1,34 @@
+import { Component, PropsWithChildren } from "react";
+
+class RootErrorBoundary extends Component<PropsWithChildren> {
+  state: { hasError: boolean };
+
+  constructor(props: PropsWithChildren) {
+    super(props);
+
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch() {
+    // reload page after 10s
+    setTimeout(window.location.reload, 10000);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ color: "red" }}>
+          <h1>ERROR!</h1>
+          <p>Page will reload after 10 seconds.</p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+export default RootErrorBoundary;
