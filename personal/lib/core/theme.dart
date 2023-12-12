68f98fc1d50c9bd6f6ce7d64a6a5ad0b0b5a9738
Cano <developer.sasazono@gmail.com>
2023-12-12 16:25:37 +0900

    upgrade google_fonts

--- personal/lib/core/theme.dart
+++ personal/lib/core/theme.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData _createTheme({
   required ThemeMode mode,
 }) {
-  return ThemeData(
-    // フォントに Noto Sans JP を使用
-    fontFamily: "Noto Sans JP",
-
+  final baseTheme = ThemeData(
     // Material 3
     useMaterial3: true,
     colorSchemeSeed: const Color(0x005c6bc0),
 
     brightness: mode == ThemeMode.dark ? Brightness.dark : Brightness.light,
+  );
+
+  return baseTheme.copyWith(
+    textTheme: GoogleFonts.notoSansJpTextTheme(baseTheme.textTheme),
   );
 }
 
