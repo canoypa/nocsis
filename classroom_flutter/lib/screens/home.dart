08119c809e7c613965904501072cb6bfcd363f9d
Cano <developer.sasazono@gmail.com>
2023-12-30 21:43:42 +0900

    auth

--- classroom_flutter/lib/screens/home.dart
+++ classroom_flutter/lib/screens/home.dart
@@ -1,30 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nocsis_classroom/components/clock.dart';
 import 'package:nocsis_classroom/components/weather_graph.dart';
+import 'package:nocsis_classroom/providers/auth.dart';
+import 'package:nocsis_classroom/screens/signin.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        const WeatherGraph(),
-        Padding(
-          padding: EdgeInsets.all(24.r),
-          child: Row(
-            children: [
-              const Expanded(child: HomeInfo()),
-              SizedBox(width: 24.r),
-              const Expanded(child: HomeSchedules()),
-            ],
-          ),
-        ),
-      ],
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(authProvider);
+
+    return user.when(
+      data: (user) {
+        final signedIn = user != null;
+
+        if (!signedIn) {
+          return const SignInScreen();
+        }
+
+        return Stack(
+          children: [
+            const WeatherGraph(),
+            Padding(
+              padding: EdgeInsets.all(24.r),
+              child: Row(
+                children: [
+                  const Expanded(child: HomeInfo()),
+                  SizedBox(width: 24.r),
+                  const Expanded(child: HomeSchedules()),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+      loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+      error: (e, s) {
+        print(e);
+
+        return const Center(
+          child: Text("エラーが発生しました"),
+        );
+      },
     );
   }
 }
