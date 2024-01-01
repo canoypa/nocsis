84c2114521c4961effeddfe0ac4a066ec4f87cda
Cano <developer.sasazono@gmail.com>
2024-01-01 18:22:05 +0900

    update

--- classroom_flutter/lib/components/home_schedules.dart
+++ classroom_flutter/lib/components/home_schedules.dart
@@ -1,20 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
+import 'package:nocsis_classroom/providers/classes.dart';
+import 'package:nocsis_classroom/providers/events.dart';
 
-class HomeSchedules extends StatelessWidget {
+class HomeSchedules extends ConsumerWidget {
   const HomeSchedules({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final classes = ref.watch(classesProvider).whenOrNull(data: (data) => data);
+    final events = ref.watch(eventsProvider).whenOrNull(data: (data) => data);
+
+    if (classes == null || events == null) {
+      return const SizedBox();
+    }
+
+    if (classes.items.isNotEmpty) {
+      return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24.sp),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16.sp),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "今日の授業",
+                style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    ),
+              ),
+              ...classes.items.map(
+                (e) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        e.title,
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      Text(
+                        "${e.period}時間目",
+                        style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSurfaceVariant,
+                            ),
+                      ),
+                    ],
+                  );
+                },
+              ).toList()
+            ],
+          ),
+        ),
+      );
+    }
+
+    final eventDateFormatter = DateFormat('M月dd日');
     return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(24.sp),
+      ),
       child: Padding(
         padding: EdgeInsets.all(16.sp),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "まるまるの日",
-              style: Theme.of(context).textTheme.bodyMedium,
+              "今後のイベント",
+              style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
             ),
+            ...events.items.map(
+              (e) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      e.title,
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                    Text(
+                      eventDateFormatter.format(e.startAt),
+                      style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.onSurfaceVariant,
+                          ),
+                    ),
+                  ],
+                );
+              },
+            ).toList()
           ],
         ),
       ),
