10a140a74c85c4476dcc17a7c6240492460eb3bc
Cano <developer.sasazono@gmail.com>
2024-01-01 19:50:33 +0900

    during class

--- classroom_flutter/lib/screens/during_class.dart
+++ classroom_flutter/lib/screens/during_class.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nocsis_classroom/components/during_class_progress.dart';
+import 'package:nocsis_classroom/core/cron.dart';
+import 'package:nocsis_classroom/models/classes.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class DuringClassScreen extends StatelessWidget {
+part 'during_class.g.dart';
+
+@riverpod
+Stream<void> updatePerMinutes(_) async* {
+  final schedule = Cron.parse("* * * * *");
+
+  while (true) {
+    final now = DateTime.now();
+    await Future.delayed(schedule.next(now).difference(now));
+  }
+}
+
+class DuringClassScreen extends ConsumerWidget {
+  final ClassData? data;
+
   const DuringClassScreen({
     super.key,
+    required this.data,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(updatePerMinutesProvider);
+
+    final classData = data;
+
+    if (classData == null) {
+      return const SizedBox();
+    }
+
+    //残り時間を分で計算
+    final classTerm = classData.endAt.difference(classData.startAt);
+    final remainingTime = classTerm.inMinutes -
+        DateTime.now().difference(classData.startAt).inMinutes;
+
     return Stack(
       children: [
-        DuringClassProgress(),
+        DuringClassProgress(data: classData),
         Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
@@ -18,12 +50,21 @@ class DuringClassScreen extends StatelessWidget {
               Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Text("1時間目", style: Theme.of(context).textTheme.titleSmall),
+                  Text(
+                    "${classData.period}時間目",
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
                   const Text("・"),
-                  Text("授業名", style: Theme.of(context).textTheme.titleSmall),
+                  Text(
+                    classData.title,
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
                 ],
               ),
-              Text("90分", style: Theme.of(context).textTheme.displayLarge),
+              Text(
+                "$remainingTime分",
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
             ],
           ),
         ),
--- classroom_flutter/lib/screens/home.dart
+++ classroom_flutter/lib/screens/home.dart
@@ -5,6 +5,8 @@ import 'package:nocsis_classroom/components/home_info.dart';
 import 'package:nocsis_classroom/components/home_schedules.dart';
 import 'package:nocsis_classroom/components/weather_graph.dart';
 import 'package:nocsis_classroom/providers/auth.dart';
+import 'package:nocsis_classroom/providers/during_class_data.dart';
+import 'package:nocsis_classroom/screens/during_class.dart';
 import 'package:nocsis_classroom/screens/signin.dart';
 
 class HomeScreen extends ConsumerWidget {
@@ -24,21 +26,7 @@ class HomeScreen extends ConsumerWidget {
           return const SignInScreen();
         }
 
-        return Stack(
-          children: [
-            const WeatherGraph(),
-            Padding(
-              padding: EdgeInsets.all(24.r),
-              child: Row(
-                children: [
-                  const Expanded(child: HomeInfo()),
-                  SizedBox(width: 24.r),
-                  const Expanded(child: HomeSchedules()),
-                ],
-              ),
-            ),
-          ],
-        );
+        return _Home();
       },
       loading: () {
         return const Center(
@@ -52,6 +40,44 @@ class HomeScreen extends ConsumerWidget {
           child: Text("エラーが発生しました"),
         );
       },
+    );
+  }
+}
+
+class _Home extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final duringClassData = ref.watch(duringClassDataProvider).whenOrNull(
+          data: (data) => data,
+        );
+
+    return Stack(
+      children: [
+        AnimatedOpacity(
+          opacity: duringClassData == null ? 1 : 0,
+          duration: const Duration(milliseconds: 300),
+          child: Stack(
+            children: [
+              const WeatherGraph(),
+              Padding(
+                padding: EdgeInsets.all(24.r),
+                child: Row(
+                  children: [
+                    const Expanded(child: HomeInfo()),
+                    SizedBox(width: 24.r),
+                    const Expanded(child: HomeSchedules()),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        AnimatedOpacity(
+          opacity: duringClassData == null ? 0 : 1,
+          duration: const Duration(milliseconds: 300),
+          child: DuringClassScreen(data: duringClassData),
+        ),
+      ],
     );
   }
 }
