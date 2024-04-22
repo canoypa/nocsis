6aeea04ab53a9cd56e6441d0fa4d266343d6888f
Cano <41741040+canoypa@users.noreply.github.com>
2024-04-22 21:34:27 +0900

    remove v2 api

--- functions/src/service/index.ts
+++ functions/src/service/index.ts
@@ -1,4 +1,3 @@
 export * as blocking from "./blocking/index.js";
 export * as scheduled from "./scheduled/index.js";
-export * as v2 from "./v2/index.js";
 export * as v3 from "./v3/index.js";
--- functions/src/service/v2/dayduty/index.ts
+++
@@ -1,9 +0,0 @@
-import { region } from "firebase-functions";
-
-export const get = region("asia-northeast1")
-  .runWith({
-    memory: "512MB",
-  })
-  .https.onCall(async (...args) => {
-    return (await import("./get.js")).default(...args).catch(console.error);
-  });
