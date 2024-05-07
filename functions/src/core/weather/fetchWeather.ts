03204c538d3e7d02f8c27ec3279c6d8a00a50819
Cano <41741040+canoypa@users.noreply.github.com>
2024-05-07 21:01:41 +0900

    dont use one call api

--- functions/src/core/weather/fetchWeather.ts
+++ functions/src/core/weather/fetchWeather.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export type OpenWeatherMapOneCallResponse = {
+export type WeatherData = {
   current: {
     temp: number;
     weather: { id: number; main: string }[];
@@ -12,33 +12,69 @@ export type OpenWeatherMapOneCallResponse = {
   }[];
 };
 
-// FIXME: no any
+export type OWMCurrentWeatherData = {
+  weather: { id: number; main: string }[];
+  main: { temp: number };
+};
+
+export type OWMFiveDayWeatherForecastData = {
+  list: {
+    dt: number;
+    main: { temp: number };
+    pop: number;
+    weather: { id: number; main: string }[];
+  }[];
+};
+
+const CurrentWeatherEndpoint =
+  "https://api.openweathermap.org/data/2.5/weather";
+const FiveDayWeatherForecastEndpoint =
+  "https://api.openweathermap.org/data/2.5/forecast";
+
 /**
  * OpenWeatherMap API から天気情報を取得する
  */
-export const fetchWeather =
-  async (): Promise<OpenWeatherMapOneCallResponse> => {
-    const url = "https://api.openweathermap.org/data/2.5/onecall";
+export const fetchWeather = async (): Promise<WeatherData> => {
+  if (
+    !process.env.OPENWEATHERMAP_TOKEN ||
+    !process.env.WEATHER_POINT_LAT ||
+    !process.env.WEATHER_POINT_LON
+  ) {
+    throw new Error("Can not read OpenWeatherMap Environment Variables");
+  }
 
-    if (
-      !process.env.OPENWEATHERMAP_TOKEN ||
-      !process.env.WEATHER_POINT_LAT ||
-      !process.env.WEATHER_POINT_LON
-    ) {
-      throw new Error("Can not read OpenWeatherMap Environment Variables");
-    }
-
-    const params = {
-      appid: process.env.OPENWEATHERMAP_TOKEN,
-      lat: process.env.WEATHER_POINT_LAT,
-      lon: process.env.WEATHER_POINT_LON,
-      units: "metric",
-      exclude: "current,minutely,daily,alerts",
-    };
-
-    const { data } = await axios.get<OpenWeatherMapOneCallResponse>(url, {
-      params,
-    });
-
-    return data;
+  const baseParams = {
+    appid: process.env.OPENWEATHERMAP_TOKEN,
+    lat: process.env.WEATHER_POINT_LAT,
+    lon: process.env.WEATHER_POINT_LON,
+    units: "metric",
   };
+
+  const forecastParams = {
+    ...baseParams,
+    cnt: 8,
+  };
+
+  const [{ data: currentData }, { data: forecastData }] = await Promise.all([
+    axios.get<OWMCurrentWeatherData>(CurrentWeatherEndpoint, {
+      params: baseParams,
+    }),
+    axios.get<OWMFiveDayWeatherForecastData>(FiveDayWeatherForecastEndpoint, {
+      params: forecastParams,
+    }),
+  ]);
+
+  const data: WeatherData = {
+    current: {
+      temp: currentData.main.temp,
+      weather: currentData.weather,
+    },
+    hourly: forecastData.list.map((v) => ({
+      temp: v.main.temp,
+      pop: v.pop,
+      weather: v.weather,
+    })),
+  };
+
+  return data;
+};
