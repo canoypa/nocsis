1fe2276c880b28d4dee9850b741e9d5e52bde900
Cano <41741040+canoypa@users.noreply.github.com>
2024-08-17 23:03:54 +0900

    ディスプレイ内の認証チェックを削除

--- classroom/lib/screens/home.dart
+++ classroom/lib/screens/home.dart
@@ -1,53 +1,17 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nocsis_classroom/components/home_info.dart';
 import 'package:nocsis_classroom/components/home_schedules.dart';
 import 'package:nocsis_classroom/components/weather_graph.dart';
-import 'package:nocsis_classroom/providers/auth.dart';
 import 'package:nocsis_classroom/providers/during_class_data.dart';
 import 'package:nocsis_classroom/screens/during_class.dart';
-import 'package:nocsis_classroom/screens/signin.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({
     super.key,
   });
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final user = ref.watch(authProvider);
-
-    return user.when(
-      data: (user) {
-        final signedIn = user != null;
-
-        if (!signedIn) {
-          return const SignInScreen();
-        }
-
-        return _Home();
-      },
-      loading: () {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-      error: (e, s) {
-        if (kDebugMode) {
-          print(e);
-        }
-
-        return const Center(
-          child: Text("エラーが発生しました"),
-        );
-      },
-    );
-  }
-}
-
-class _Home extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final duringClassData = ref.watch(duringClassDataProvider).value;
