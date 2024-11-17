e2347e97672d925d31e09b9199d196a7108d5608
Cano <41741040+canoypa@users.noreply.github.com>
2024-11-17 20:44:04 +0900

    upgrade

--- frontend/lib/pages/licenses.g.dart
+++ frontend/lib/pages/licenses.g.dart
@@ -20,7 +20,9 @@ final licensesProvider =
   allTransitiveDependencies: null,
 );
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 typedef LicensesRef
     = AutoDisposeFutureProviderRef<Map<String, List<LicenseParagraph>>>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
