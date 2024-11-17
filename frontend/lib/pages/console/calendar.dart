80daa1e0f39d0eba33aa0fc76f7fdd5e1e2398b1
Cano <41741040+canoypa@users.noreply.github.com>
2024-11-17 16:40:52 +0900

    transition

--- frontend/lib/pages/console/calendar.dart
+++ frontend/lib/pages/console/calendar.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -6,9 +7,16 @@ class ConsoleCalendarRoute extends GoRouteData {
 
   @override
   Page<void> buildPage(BuildContext context, GoRouterState state) {
-    return MaterialPage(
+    return CustomTransitionPage(
       key: state.pageKey,
       child: const ConsoleCalendarPage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return FadeThroughTransition(
+          animation: animation,
+          secondaryAnimation: secondaryAnimation,
+          child: child,
+        );
+      },
     );
   }
 }
