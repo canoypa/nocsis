5d861422a49392cccb70c972b9d310412f896a32
Cano <41741040+canoypa@users.noreply.github.com>
2024-12-08 21:19:24 +0900

    update

--- frontend/lib/pages/settings/sign_in.dart
+++ frontend/lib/pages/settings/sign_in.dart
@@ -2,7 +2,6 @@ import 'package:animations/animations.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class SettingsSignInRoute extends GoRouteData {
   const SettingsSignInRoute();
