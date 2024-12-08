5418345e51022cd2a929ecce977393fa9bfc7cf2
Cano <41741040+canoypa@users.noreply.github.com>
2024-12-08 15:45:56 +0900

    test

--- frontend/lib/pages/settings/sign_in.dart
+++ frontend/lib/pages/settings/sign_in.dart
@@ -1,6 +1,8 @@
 import 'package:animations/animations.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SettingsSignInRoute extends GoRouteData {
   const SettingsSignInRoute();
@@ -40,6 +42,30 @@ class SettingsSignInPage extends StatelessWidget {
               style: Theme.of(context).textTheme.displayMedium,
             ),
             const SizedBox(height: 48),
+            const Text('Google'),
+            TextButton(
+              onPressed: () async {
+                final googleUser = await GoogleSignIn(
+                  clientId:
+                      '219141289630-1duqb26tbk7dte61804hjqg64qmhfsc2.apps.googleusercontent.com',
+                ).signIn();
+                if (googleUser == null) return;
+
+                final authentication = await googleUser.authentication;
+
+                final newCredential = GoogleAuthProvider.credential(
+                  accessToken: authentication.accessToken,
+                  idToken: authentication.idToken,
+                );
+
+                await FirebaseAuth.instance.currentUser!
+                    .unlink(GoogleAuthProvider.PROVIDER_ID);
+
+                await FirebaseAuth.instance.currentUser!
+                    .linkWithCredential(newCredential);
+              },
+              child: const Text("別アカウントと連携する"),
+            ),
           ],
         ),
       ),
