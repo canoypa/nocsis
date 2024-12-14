2e4ed61732c03051e50ca0de196b9ab39003bf66
Cano <41741040+canoypa@users.noreply.github.com>
2024-12-14 23:44:49 +0900

    wip

--- frontend/lib/pages/settings/index.dart
+++ frontend/lib/pages/settings/index.dart
@@ -1,4 +1,5 @@
 import 'package:animations/animations.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -40,9 +41,83 @@ class SettingsTopPage extends StatelessWidget {
               style: Theme.of(context).textTheme.displayMedium,
             ),
             const SizedBox(height: 48),
+            Text(
+              'アカウント',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 24),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'メールアドレス',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const SizedBox(height: 4),
+                    Text(FirebaseAuth.instance.currentUser?.email ?? ''),
+                  ],
+                ),
+                const Spacer(),
+                OutlinedButton(
+                  onPressed: () {
+                    // TODO
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return const ChangeEmailDialog();
+                        });
+                  },
+                  child: const Text('変更する'),
+                ),
+              ],
+            )
           ],
         ),
       ),
+    );
+  }
+}
+
+class ChangeEmailDialog extends StatelessWidget {
+  const ChangeEmailDialog({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
+
+    return AlertDialog(
+      title: const Text('メールアドレスを変更'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: emailController,
+            decoration: const InputDecoration(
+              labelText: '新しいメールアドレス',
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('キャンセル'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            FirebaseAuth.instance.currentUser?.verifyBeforeUpdateEmail(
+              emailController.text,
+            );
+          },
+          child: const Text('変更する'),
+        ),
+      ],
     );
   }
 }
