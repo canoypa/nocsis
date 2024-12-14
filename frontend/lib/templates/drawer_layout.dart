eccd682300b3a9f8be21ef208a5336c0deeb5158
Cano <41741040+canoypa@users.noreply.github.com>
2024-12-14 22:57:29 +0900

    new layout

--- frontend/lib/templates/drawer_layout.dart
+++ frontend/lib/templates/drawer_layout.dart
@@ -51,17 +51,25 @@ class DrawerLayout extends StatelessWidget {
             children: navigationItems,
           ),
           Expanded(
-            child: Container(
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.surface,
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(32),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: const BorderRadius.all(Radius.circular(28)),
                 ),
-              ),
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.all(64),
-                child: child,
+                child: LayoutBuilder(builder: (context, constraints) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      constraints: BoxConstraints(
+                        minHeight: constraints.maxHeight,
+                      ),
+                      padding: const EdgeInsets.all(64),
+                      child: child,
+                    ),
+                  );
+                }),
               ),
             ),
           ),
