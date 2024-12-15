d9d8a1943e44e41c39cfb1b6e765e38dbae37386
Cano <41741040+canoypa@users.noreply.github.com>
2024-12-15 20:16:04 +0900

    いい感じにする

--- frontend/lib/pages/settings/index.dart
+++ frontend/lib/pages/settings/index.dart
@@ -66,26 +66,7 @@ class SettingsTopPage extends StatelessWidget {
                     showDialog(
                       context: context,
                       builder: (context) {
-                        return Dialog(
-                          child: SignInForm(
-                            title: "メールアドレスを変更する",
-                            description: '始めに、現在のアカウントで再認証が必要です。',
-                            onGoogleSignIn: () async {
-                              await FirebaseAuth.instance.currentUser
-                                  ?.reauthenticateWithPopup(
-                                      GoogleAuthProvider());
-
-                              Navigator.of(context).pop();
-
-                              showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return const ChangeEmailDialog();
-                                },
-                              );
-                            },
-                          ),
-                        );
+                        return const ChangeEmailDialog();
                       },
                     );
                   },
@@ -100,66 +81,100 @@ class SettingsTopPage extends StatelessWidget {
   }
 }
 
-class ChangeEmailDialog extends StatelessWidget {
-  const ChangeEmailDialog({
-    super.key,
-  });
+class ChangeEmailDialog extends StatefulWidget {
+  const ChangeEmailDialog({super.key});
+
+  @override
+  ChangeEmailDialogState createState() => ChangeEmailDialogState();
+}
+
+class ChangeEmailDialogState extends State<ChangeEmailDialog> {
+  bool reAuthenticated = false;
 
   @override
   Widget build(BuildContext context) {
+    return Dialog(
+      child: PageTransitionSwitcher(
+        transitionBuilder: (child, primaryAnimation, secondaryAnimation) {
+          return SharedAxisTransition(
+            animation: primaryAnimation,
+            secondaryAnimation: secondaryAnimation,
+            transitionType: SharedAxisTransitionType.horizontal,
+            child: child,
+          );
+        },
+        child: !reAuthenticated ? _signInForm() : _changeEmailForm(),
+      ),
+    );
+  }
+
+  _signInForm() {
+    return SignInForm(
+      title: "メールアドレスを変更する",
+      description: '始めに、現在のアカウントで再認証が必要です。',
+      onGoogleSignIn: () async {
+        await FirebaseAuth.instance.currentUser
+            ?.reauthenticateWithPopup(GoogleAuthProvider());
+
+        setState(() {
+          reAuthenticated = true;
+        });
+      },
+    );
+  }
+
+  _changeEmailForm() {
     final TextEditingController emailController = TextEditingController();
 
-    return Dialog(
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(28),
-          color: Theme.of(context).colorScheme.surfaceContainerHigh,
-        ),
-        constraints: const BoxConstraints(maxWidth: 800),
-        padding: const EdgeInsets.all(64),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Text(
-                'メールアドレスを変更する',
-                style: Theme.of(context).textTheme.displaySmall,
-              ),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        color: Theme.of(context).colorScheme.surfaceContainerHigh,
+      ),
+      constraints: const BoxConstraints(maxWidth: 800),
+      padding: const EdgeInsets.all(64),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Text(
+              'メールアドレスを変更する',
+              style: Theme.of(context).textTheme.displaySmall,
             ),
-            const SizedBox(height: 48),
-            const Text(
-                '1. 新しいメールアドレスを入力して、「確認する」を押してください。\n2. 入力したメールアドレス宛に確認のメールが届きます。メールに含まれるリンクを開いて、メールアドレスの変更を完了してください。'),
-            const SizedBox(height: 8),
-            const Text(
-                '※ 連携している Google アカウントは変更されません。\n※ 「新しいメールアドレスでログインできるようになりました」と表示されますが、現在 Google アカウント以外でログインする方法はないため、この内容は無視してください。'),
-            const SizedBox(height: 48),
-            TextField(
-              autofocus: true,
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: '新しいメールアドレス',
-                hintText: 'yourname@gmail.com',
-                border: OutlineInputBorder(),
-              ),
+          ),
+          const SizedBox(height: 48),
+          const Text(
+              '1. 新しいメールアドレスを入力して、「確認する」を押してください。\n2. 入力したメールアドレス宛に確認のメールが届きます。メールに含まれるリンクを開いて、メールアドレスの変更を完了してください。'),
+          const SizedBox(height: 8),
+          const Text(
+              '※ 連携している Google アカウントは変更されません。\n※ 「新しいメールアドレスでログインできるようになりました」と表示されますが、現在 Google アカウント以外でログインする方法はないため、この内容は無視してください。'),
+          const SizedBox(height: 48),
+          TextField(
+            autofocus: true,
+            controller: emailController,
+            decoration: const InputDecoration(
+              labelText: '新しいメールアドレス',
+              hintText: 'yourname@gmail.com',
+              border: OutlineInputBorder(),
             ),
-            const SizedBox(height: 24),
-            SizedBox(
-              width: double.infinity,
-              child: FilledButton(
-                onPressed: () async {
-                  await FirebaseAuth.instance.currentUser
-                      ?.verifyBeforeUpdateEmail(
-                    emailController.text,
-                  );
+          ),
+          const SizedBox(height: 24),
+          SizedBox(
+            width: double.infinity,
+            child: FilledButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.currentUser
+                    ?.verifyBeforeUpdateEmail(
+                  emailController.text,
+                );
 
-                  Navigator.of(context).pop();
-                },
-                child: const Text('確認する'),
-              ),
+                Navigator.of(context).pop();
+              },
+              child: const Text('確認する'),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
