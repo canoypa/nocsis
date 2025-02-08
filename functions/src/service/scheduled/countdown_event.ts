77790547d37288fae3aeaa1d33a5e590479e8d95
Cano <41741040+canoypa@users.noreply.github.com>
2025-02-08 16:34:10 +0900

    整理

--- functions/src/service/scheduled/countdown_event.ts
+++ functions/src/service/scheduled/countdown_event.ts
@@ -70,27 +70,23 @@ export const isCountdownTarget = (
     zone: "Asia/Tokyo",
   }).startOf("day");
 
-  // オプションが指定されていない場合
-  if (!match.before || !match.unit) {
-    const isBeforeEventStart =
-      Math.floor(eventStartDate.diff(timestamp, "days").days) > 0;
-    return isBeforeEventStart;
+  // オプションが指定されている場合
+  if (match.before && match.unit) {
+    const countdownStartDate = eventStartDate.minus({
+      [match.unit]: match.before,
+    });
+
+    // カウントダウン開始日より前の場合、カウントダウン対象ではない
+    const isBeforeCountdownStart =
+      countdownStartDate.diff(timestamp, "days").days > 0;
+    if (isBeforeCountdownStart) {
+      return false;
+    }
   }
 
-  const countdownStartDate = eventStartDate.minus({
-    [match.unit]: match.before,
-  });
-
-  // 現在日時がカウントダウン開始日~イベント開始日前日の間にあるか
-  const isBeforeCountdownStart =
-    Math.floor(countdownStartDate.diff(timestamp, "days").days) <= 0;
-  const isBeforeEventStart =
-    Math.floor(eventStartDate.diff(timestamp, "days").days) > 0;
-  if (isBeforeCountdownStart && isBeforeEventStart) {
-    return true;
-  }
-
-  return false;
+  // イベント以前日より前の場合、カウントダウン対象
+  const isBeforeEventStart = eventStartDate.diff(timestamp, "days").days > 0;
+  return isBeforeEventStart;
 };
 
 export const removeCountdownPattern = (text: string): string => {
