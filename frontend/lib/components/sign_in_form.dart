4d1679e6167a83732d4703a0eb86f850b01def66
Cano <41741040+canoypa@users.noreply.github.com>
2025-03-08 22:38:37 +0900

    よしなにする

--- frontend/lib/components/sign_in_form.dart
+++ frontend/lib/components/sign_in_form.dart
@@ -42,6 +42,85 @@ class _SignInFormState extends State<SignInForm> {
     }
   }
 
+  Widget _intro() {
+    return Expanded(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset("assets/images/Icon-192.png", width: 64),
+          const SizedBox(height: 16),
+          Text(widget.title, style: Theme.of(context).textTheme.displaySmall),
+          const SizedBox(height: 24),
+          Text(
+            widget.description,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _form() {
+    return Expanded(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        spacing: 24,
+        children: [
+          OutlinedButton(
+            onPressed: widget.onGoogleSignIn,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset("assets/images/google.svg", width: 18),
+                const SizedBox(width: 8),
+                const Text("Google で続行"),
+              ],
+            ),
+          ),
+          Text(
+            'または',
+            style: Theme.of(context).textTheme.labelMedium?.copyWith(
+              color: Theme.of(context).colorScheme.onSurfaceVariant,
+            ),
+          ),
+          TextField(
+            controller: emailFieldController,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'メールアドレス',
+            ),
+          ),
+          TextField(
+            controller: passwordFieldController,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              labelText: 'パスワード',
+              suffixIcon: IconButton(
+                onPressed: _togglePasswordVisibility,
+                icon: Icon(_passwordVisibilityIcon()),
+              ),
+            ),
+            obscureText: invisiblePassword,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: FilledButton(
+              onPressed: () {
+                widget.onPasswordSignIn(
+                  emailFieldController.text,
+                  passwordFieldController.text,
+                );
+              },
+              child: const Text('サインイン'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -53,88 +132,8 @@ class _SignInFormState extends State<SignInForm> {
       padding: const EdgeInsets.all(64),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset("assets/images/Icon-192.png", width: 64),
-                const SizedBox(height: 16),
-                Text(
-                  widget.title,
-                  style: Theme.of(context).textTheme.displaySmall,
-                ),
-                const SizedBox(height: 24),
-                Text(
-                  widget.description,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(width: 48),
-          Expanded(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                OutlinedButton(
-                  onPressed: widget.onGoogleSignIn,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset("assets/images/google.svg", width: 18),
-                      const SizedBox(width: 8),
-                      const Text("Google で続行"),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 24),
-                Text(
-                  'または',
-                  style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                    color: Theme.of(context).colorScheme.onSurfaceVariant,
-                  ),
-                ),
-                const SizedBox(height: 24),
-                TextField(
-                  controller: emailFieldController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'メールアドレス',
-                  ),
-                ),
-                const SizedBox(height: 24),
-                TextField(
-                  controller: passwordFieldController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: 'パスワード',
-                    suffixIcon: IconButton(
-                      onPressed: _togglePasswordVisibility,
-                      icon: Icon(_passwordVisibilityIcon()),
-                    ),
-                  ),
-                  obscureText: invisiblePassword,
-                ),
-                const SizedBox(height: 24),
-                SizedBox(
-                  width: double.infinity,
-                  child: FilledButton(
-                    onPressed: () {
-                      widget.onPasswordSignIn(
-                        emailFieldController.text,
-                        passwordFieldController.text,
-                      );
-                    },
-                    child: const Text('サインイン'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        spacing: 48,
+        children: [_intro(), _form()],
       ),
     );
   }
