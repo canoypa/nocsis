35fb6a23d97437801fd76fb31b12f7a901fe5e05
Cano <41741040+canoypa@users.noreply.github.com>
2025-05-18 15:34:54 +0900

    update openapi output dir

--- backend/src/generate_openapi_spec.ts
+++ backend/src/generate_openapi_spec.ts
@@ -1,8 +1,10 @@
 import fs from "node:fs";
+import { resolve } from "node:path";
 import type { Hono } from "hono";
 import { generateSpecs } from "hono-openapi";
 
 export const generateOpenapiSpec = async (app: Hono) => {
   const spec = await generateSpecs(app);
-  fs.writeFileSync("openapi.json", JSON.stringify(spec, null, 2));
+  const path = resolve("../api-spec.openapi.json");
+  fs.writeFileSync(path, JSON.stringify(spec, null, 2));
 };
