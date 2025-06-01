b8fe8078d905aef18ecc10f6eb4b35a2fb4277a5
Cano <41741040+canoypa@users.noreply.github.com>
2025-06-01 20:47:03 +0900

    user test helper

---
+++ backend/tests/helpers/users.ts
@@ -0,0 +1,36 @@
+import { type UserRecord, getAuth } from "firebase-admin/auth";
+import { z } from "zod";
+
+const LoginResultSchema = z.object({
+  idToken: z.string(),
+  refreshToken: z.string(),
+  expiresIn: z.string(),
+});
+export type LoginResult = z.infer<typeof LoginResultSchema>;
+
+export const login = async (user: UserRecord) => {
+  const auth = getAuth();
+
+  const customToken = await auth.createCustomToken(user.uid);
+
+  const res = await fetch(
+    "http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=dummy",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: customToken,
+        returnSecureToken: true,
+      }),
+    },
+  ).then((res) => res.json());
+
+  const parsed = LoginResultSchema.safeParse(res);
+  if (!parsed.success) {
+    throw new Error("Failed to parse login response");
+  }
+
+  return parsed.data;
+};
