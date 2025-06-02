5b231f267c002bebe5f2b0e33552ec395187d541
Cano <41741040+canoypa@users.noreply.github.com>
2025-06-02 22:34:45 +0900

    update

--- backend/src/controllers/v1/group_controller.ts
+++ backend/src/controllers/v1/group_controller.ts
@@ -36,152 +36,156 @@ const groupSchema = z
   })
   .openapi({ description: "グループ" });
 
-const groupGetParamSchema = z.object({
+const paramSchema = z.object({
   id: z.string().openapi({ description: "ID" }),
 });
 
-const patchGroupSchema = groupSchema.omit({ id: true }).partial();
+const groupPatchBodySchema = groupSchema.omit({ id: true }).partial();
 
-groupRoutes
-  .get(
-    "/:id",
-    describeRoute({
-      description: "グループを取得する",
-      responses: {
-        200: {
-          description: "Successful response",
-          content: {
-            "application/json": { schema: resolver(groupSchema) },
-          },
-        },
-        401: {
-          description: "Unauthorized",
-        },
-        403: {
-          description: "Forbidden",
-        },
-        404: {
-          description: "Not Found",
-        },
-        500: {
-          description: "Internal Server Error",
+groupRoutes.get(
+  "/:id",
+  describeRoute({
+    description: "グループを取得する",
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "application/json": { schema: resolver(groupSchema) },
         },
       },
-      security: [{ bearer: [] }],
-      validateResponse: true,
-    }),
-    validator("param", groupGetParamSchema),
-    authentication,
-    async (c) => {
-      const firestore = getFirestore(firebaseApp);
-
-      const user = getContext<AuthenticatedEnv>().get("currentUser");
-
-      const groupSnapshot = await firestore
-        .collection("groups")
-        .doc(c.req.param("id"))
-        .get();
-
-      if (!groupSnapshot.exists) {
-        throw new HTTPException(404, { message: "グループが存在しません。" });
-      }
-
-      const userJoinedGroupSnapshot = await firestore
-        .collection("user_joined_groups")
-        .where("user_id", "==", user.uid)
-        .where("group_id", "==", c.req.param("id"))
-        .get();
-
-      if (userJoinedGroupSnapshot.empty) {
-        throw new HTTPException(403, {
-          message: "グループに参加していません。",
-        });
-      }
-
-      const group = groupSnapshot.data();
-      assert(group);
-
-      return c.json({
-        id: groupSnapshot.id,
-        ...group,
-      });
+      401: {
+        description: "Unauthorized",
+      },
+      403: {
+        description: "Forbidden",
+      },
+      404: {
+        description: "Not Found",
+      },
+      500: {
+        description: "Internal Server Error",
+      },
     },
-  )
-  .post(async (_c) => {})
-  .patch(
-    "/:id",
-    describeRoute({
-      description: "グループを更新する",
-      responses: {
-        200: {
-          description: "Successful response",
-          content: {
-            "application/json": { schema: resolver(groupSchema) },
-          },
-        },
-        400: {
-          description: "Bad Request",
-        },
-        401: {
-          description: "Unauthorized",
-        },
-        403: {
-          description: "Forbidden",
-        },
-        404: {
-          description: "Not Found",
-        },
-        500: {
-          description: "Internal Server Error",
+    security: [{ bearer: [] }],
+    validateResponse: true,
+  }),
+  validator("param", paramSchema),
+  authentication,
+  async (c) => {
+    const firestore = getFirestore(firebaseApp);
+
+    const groupId = c.req.param("id");
+    const user = getContext<AuthenticatedEnv>().get("currentUser");
+
+    const groupSnapshot = await firestore
+      .collection("groups")
+      .doc(groupId)
+      .get();
+
+    if (!groupSnapshot.exists) {
+      throw new HTTPException(404, { message: "グループが存在しません。" });
+    }
+
+    const userJoinedGroupSnapshot = await firestore
+      .collection("user_joined_groups")
+      .where("user_id", "==", user.uid)
+      .where("group_id", "==", groupId)
+      .get();
+
+    if (userJoinedGroupSnapshot.empty) {
+      throw new HTTPException(403, {
+        message: "グループに参加していません。",
+      });
+    }
+
+    const group = groupSnapshot.data();
+    assert(group);
+
+    return c.json({
+      id: groupSnapshot.id,
+      ...group,
+    });
+  },
+);
+
+groupRoutes.post(async (_c) => {});
+
+groupRoutes.patch(
+  "/:id",
+  describeRoute({
+    description: "グループを更新する",
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "application/json": { schema: resolver(groupSchema) },
         },
       },
-      security: [{ bearer: [] }],
-      validateResponse: true,
-    }),
-    validator("json", patchGroupSchema),
-    authentication,
-    async (c) => {
-      const firestore = getFirestore(firebaseApp);
-
-      const groupId = c.req.param("id");
-      const user = getContext<AuthenticatedEnv>().get("currentUser");
-      const data = c.req.valid("json");
-
-      console.log(data);
-
-      if (!data || Object.keys(data).length === 0) {
-        throw new HTTPException(400, { message: "不正なデータです。" });
-      }
-
-      const groupRef = firestore.collection("groups").doc(groupId);
-
-      const groupSnapshot = await groupRef.get();
-      if (!groupSnapshot.exists) {
-        throw new HTTPException(404, { message: "グループが存在しません。" });
-      }
-
-      const userJoinedGroupSnapshot = await firestore
-        .collection("user_joined_groups")
-        .where("user_id", "==", user.uid)
-        .where("group_id", "==", groupId)
-        .get();
-
-      if (userJoinedGroupSnapshot.empty) {
-        throw new HTTPException(403, {
-          message: "グループに参加していません。",
-        });
-      }
-
-      await groupRef.update(data);
-
-      const updatedGroupSnapshot = await groupRef.get();
-      const updatedGroup = updatedGroupSnapshot.data();
-      assert(updatedGroup);
-
-      return c.json({
-        id: updatedGroupSnapshot.id,
-        ...updatedGroup,
-      });
+      400: {
+        description: "Bad Request",
+      },
+      401: {
+        description: "Unauthorized",
+      },
+      403: {
+        description: "Forbidden",
+      },
+      404: {
+        description: "Not Found",
+      },
+      500: {
+        description: "Internal Server Error",
+      },
     },
-  )
-  .delete(async (_c) => {});
+    security: [{ bearer: [] }],
+    validateResponse: true,
+  }),
+  validator("param", paramSchema),
+  validator("json", groupPatchBodySchema),
+  authentication,
+  async (c) => {
+    const firestore = getFirestore(firebaseApp);
+
+    const groupId = c.req.param("id");
+    const user = getContext<AuthenticatedEnv>().get("currentUser");
+    const data = c.req.valid("json");
+
+    console.log(data);
+
+    if (!data || Object.keys(data).length === 0) {
+      throw new HTTPException(400, { message: "不正なデータです。" });
+    }
+
+    const groupRef = firestore.collection("groups").doc(groupId);
+
+    const groupSnapshot = await groupRef.get();
+    if (!groupSnapshot.exists) {
+      throw new HTTPException(404, { message: "グループが存在しません。" });
+    }
+
+    const userJoinedGroupSnapshot = await firestore
+      .collection("user_joined_groups")
+      .where("user_id", "==", user.uid)
+      .where("group_id", "==", groupId)
+      .get();
+
+    if (userJoinedGroupSnapshot.empty) {
+      throw new HTTPException(403, {
+        message: "グループに参加していません。",
+      });
+    }
+
+    await groupRef.update(data);
+
+    const updatedGroupSnapshot = await groupRef.get();
+    const updatedGroup = updatedGroupSnapshot.data();
+    assert(updatedGroup);
+
+    return c.json({
+      id: updatedGroupSnapshot.id,
+      ...updatedGroup,
+    });
+  },
+);
+
+groupRoutes.delete(async (_c) => {});
