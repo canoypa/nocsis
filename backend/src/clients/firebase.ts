790bc4d77925f04b0a875ae87805ca6a3d2471b6
Cano <41741040+canoypa@users.noreply.github.com>
2025-08-20 21:32:25 +0900

    Firebaseアプリ初期化の書き方を改善

--- backend/src/clients/firebase.ts
+++ backend/src/clients/firebase.ts
@@ -2,7 +2,6 @@ import { getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-export const firebaseApp =
-  getApps().length === 0 ? initializeApp() : getApps()[0];
+export const firebaseApp = getApps().at(0) ?? initializeApp();
 export const auth = getAuth(firebaseApp);
 export const firestore = getFirestore(firebaseApp);
