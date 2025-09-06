c3e7483010774fd8f94116a990e4d209c5bb520b
Cano <41741040+canoypa@users.noreply.github.com>
2025-09-06 11:45:55 +0900

    API移行第二弾: プロバイダーとページでの新しいAPIクライアント使用

--- frontend/lib/components/personal/event_list.dart
+++ frontend/lib/components/personal/event_list.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:nocsis/components/personal/basic_card.dart';
-import 'package:nocsis/models/events.dart';
+import 'package:nocsis/generated/api_client/api.models.swagger.dart';
 
 class EventListView extends StatelessWidget {
-  final List<EventData> items;
+  final List<Event> items;
 
   const EventListView({super.key, required this.items});
 
@@ -19,9 +19,20 @@ class EventListView extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: (items).map((data) {
+              // FIXME: swagger-dart-code-generator の制限により anyOf が dynamic 型になってしまうので、変換をかける
+              final DateTime? startDate = data.startAt is String
+                  ? DateTime.tryParse(data.startAt as String)
+                  : data.startAt is DateTime
+                  ? data.startAt as DateTime
+                  : null;
+
+              final String dateText = startDate != null
+                  ? DateFormat("M月d日").format(startDate)
+                  : "日時不明";
+
               return BasicCard(
                 primary: Text(data.title),
-                secondary: Text(DateFormat("M月d日").format(data.startAt)),
+                secondary: Text(dateText),
               );
             }).toList(),
           ),
--- frontend/lib/providers/user_joined_groups.dart
+++ frontend/lib/providers/user_joined_groups.dart
@@ -1,40 +1,18 @@
-import 'dart:async';
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:nocsis/models/user_joined_groups.dart';
+import 'package:nocsis/generated/api_client/api.models.swagger.dart';
 import 'package:nocsis/providers/api_client.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'user_joined_groups.g.dart';
 
-final fn = FirebaseFunctions.instanceFor(
-  region: "asia-northeast1",
-).httpsCallable("v4-groups-user_joined_groups-get");
-
 @riverpod
 Future<UserJoinedGroups> userJoinedGroups(Ref ref) async {
-  // 新しいAPIの呼び出しテスト
-  try {
-    final client = await ref.read(apiClientProvider.future);
-    unawaited(
-      client
-          .apiV1UsersMeGroupsGet()
-          .then((_) {})
-          .catchError((error) {
-            // ignore: avoid_print
-            print('[UserJoinedGroups] New API test error: $error');
-          }),
-    );
-  } catch (error) {
-    // ignore: avoid_print
-    print('[UserJoinedGroups] New API client initialization failed');
+  final client = await ref.read(apiClientProvider.future);
+  final response = await client.apiV1UsersMeGroupsGet();
+
+  if (response.isSuccessful && response.body != null) {
+    return response.body!;
+  } else {
+    throw Exception('User joined groups fetch failed: ${response.statusCode}');
   }
-
-  final res = await fn.call();
-
-  if (res.data == null) {
-    throw Exception("No data");
-  }
-
-  return UserJoinedGroups.fromJson(res.data);
 }
